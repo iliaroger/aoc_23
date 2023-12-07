bd5f85038db5c9c52fa0891cfdd5d265141cd305
Ilia Roger <iliailia@me.com>
2023-12-07 18:05:43 +0300

    first commit

---
+++ target/debug/deps/aco_23-e756bcfafb0f5da8.d
@@ -0,0 +1,5 @@
+/Users/ilia/Desktop/indie/rust/aoc_23/target/debug/deps/aco_23-e756bcfafb0f5da8: src/main.rs
+
+/Users/ilia/Desktop/indie/rust/aoc_23/target/debug/deps/aco_23-e756bcfafb0f5da8.d: src/main.rs
+
+src/main.rs:
